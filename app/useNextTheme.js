883f57c1eff912d7080c634bcacda72821c9dcef
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-11-06 15:29:54 +0500

    New Commit

--- app/useNextTheme.js
+++ app/useNextTheme.js
@@ -1,18 +1,18 @@
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 
-const useNextTheme = () => {
-  const { theme, setTheme, resolvedTheme } = useTheme();
+// const useNextTheme = () => {
+//   const { theme, setTheme, resolvedTheme } = useTheme();
 
-  const currentTheme = theme ?? resolvedTheme;
+//   const currentTheme = theme ?? resolvedTheme;
 
-  const toggleTheme = () => {
-    setTheme(currentTheme === "dark" ? "light" : "dark");
-  };
+//   const toggleTheme = () => {
+//     setTheme(currentTheme === "dark" ? "light" : "dark");
+//   };
 
-  return {
-    theme: currentTheme,
-    toggleTheme,
-  };
-};
+//   return {
+//     theme: currentTheme,
+//     toggleTheme,
+//   };
+// };
 
-export default useNextTheme;
+// export default useNextTheme;
